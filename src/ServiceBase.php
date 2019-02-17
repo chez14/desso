f8837f3ed40beaad0b88d50eb5829bdc935e966c
Chris <christianto.g.14@gmail.com>
2019-02-17 16:09:25 +0700

    composer and standard update

--- src/ServiceBase.php
+++ src/ServiceBase.php
@@ -2,9 +2,11 @@
 namespace Chez14\Desso;
 
 abstract class ServiceBase {
-    public abstract function get_service():String;
+    public abstract function get_service() : String;
     
     public abstract function pre_login();
 
     public abstract function post_login(String $ticket);
+
+    public abstract function validateLogin() : bool;
 }
